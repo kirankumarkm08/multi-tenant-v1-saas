8b5dee4898b2499817469942d6ae0f7f0f15bff0
kirankumarm08 <kirankumarkm1505@gmail.com>
2025-08-14 12:07:24 +0530

    fix secure https

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -41,7 +41,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const res = await fetch("http://165.227.182.17/api/tenant/login", {
+      const res = await fetch("https://165.227.182.17/api/tenant/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
